ab2d8652845a90c2121bc8e9edc5598d79c629e6
Daha <ddia4284@zenithacademy.org>
2026-02-25 14:18:12 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double PurchasePrice = 99;
+        double SalesTax = 0;
+
+        SalesTax = PurchasePrice * 0.05;
+        System.out.println("The purchase price is " + PurchasePrice);
+        System.out.println("The salex tax is $" + SalesTax);
     }
 }
